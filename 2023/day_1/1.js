a1c57b166f01a6d2c3566815a111b6ff13716f71
Nathan Krieger <nkrieger0444@gmail.com>
2023-12-01 15:53:04 -0800

    init plus first puzzle

---
+++ 2023/day_1/1.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+
+fs.open("./1.txt", "r", (err, fd) => {
+  fs.readFile(fd, "utf-8", (err, data) => {
+    const lines = data.split("\n");
+    let sum = 0;
+
+    lines.forEach((line) => {
+      let left = 0;
+      let right = line.length - 1;
+      const isDigit = (char) => /^[0-9]+$/i.test(char);
+
+      while (left < line.length) {
+        if (isDigit(line[left])) break;
+        left++;
+      }
+
+      while (right >= 0) {
+        if (isDigit(line[right])) break;
+        right--;
+      }
+
+      let calibrationValue = line[left] + line[right];
+      sum = sum += Number(calibrationValue);
+    });
+    console.log(sum);
+  });
+});
